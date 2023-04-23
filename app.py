f716f247b3c39cb8f0f5cb3fc25d0d3788df98bc
Sheiphan <sheiphanshaijan@gmail.com>
2023-04-23 16:30:35 +0530

    app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+
 # import re
 from src.components.NER import extract_pdf, KeyphraseExtractionPipeline
 from src.components.chatbot import ask_me_anything, construct_index
@@ -8,12 +9,14 @@ import os
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'textdata'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_FOLDER = "textdata"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # @app.route('/upload', methods=['POST'])
 # def upload():
@@ -21,54 +24,56 @@ def hello_world():
 #     file.save('uploaded_file.pdf')
 #     return 'File uploaded successfully'
 
-@app.route('/upload', methods=['GET','POST'])
+
+@app.route("/upload", methods=["GET", "POST"])
 def upload():
-    if request.method=='GET':
-        return render_template('upload.html')
+    if request.method == "GET":
+        return render_template("upload.html")
     else:
-        pdf_file = request.files['pdf_file']
+        pdf_file = request.files["pdf_file"]
         if pdf_file:
-            pdf_file_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf_file.filename)
+            pdf_file_path = os.path.join(app.config["UPLOAD_FOLDER"], pdf_file.filename)
 
             pdf_file.save(pdf_file_path)
-            
+
             pdf_text = extract_pdf(pdf_file_path)
             print(pdf_text)
             # print(pdf_file_path)
             # text = extract_text(str(pdf_file_path))
             # text_replace = text.replace('\n',' ')
             # return text_replace
-            
+
             # Load pipeline
             model_name = "ml6team/keyphrase-extraction-distilbert-inspec"
             extractor = KeyphraseExtractionPipeline(model=model_name)
-            
+
             keyphrases = extractor(pdf_text)
 
             print(keyphrases)
-                   
+
             # return 'PDF file uploaded and saved successfully!'
         else:
-            return 'No PDF file uploaded'
-        
-@app.route('/answer', methods=['GET', 'POST'])
-def answer():
-    if request.method == 'GET':
-        return render_template('chatbot.html')
-    
-    else:
-        
-        construct_index(r'D:\Python\Resume_NLP_Project\Resume-Strength-NLP-Project\textdata')
-        os.environ["OPENAI_API_KEY"] = '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'
+            return "No PDF file uploaded"
 
-        question = request.form['question']
-        
-        while question!='Exit':
-            answer = ask_me_anything(question)
-            question = input('Enter a question (Type Exit to Exit from the chatbot): ')
-        # Do something to get the answer based on the question
-        # answer = "The answer to your question is..."
-        return render_template('chatbot.html', answer=answer)
 
-if __name__ == '__main__':
+# @app.route("/answer", methods=["GET", "POST"])
+# def answer():
+#     if request.method == "GET":
+#         return render_template("chatbot.html")
+
+#     else:
+#         construct_index(r"textdata")
+
+#         question = request.form["question"]
+
+#         while question != "Exit":
+#             answer = ask_me_anything(question)
+#             return render_template("chatbot.html", answer=answer)
+#             question = input("Enter a question (Type Exit to Exit from the chatbot): ")
+            
+#         # Do something to get the answer based on the question
+#         # answer = "The answer to your question is..."
+
+
+if __name__ == "__main__":
     app.run(debug=True)
