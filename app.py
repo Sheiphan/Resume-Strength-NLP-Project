8de295c5f8dcfb88eab54910903ad35cf19e2ff8
Sheiphan <sheiphanshaijan@gmail.com>
2023-04-29 05:22:06 +0530

    construct_index condition

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ from transformers import AutoTokenizer, AutoModelForTokenClassification
 
 from src.components.NER import extract_pdf, ner ,KeyphraseExtractionPipeline
 from src.components.chatbot import ask_me_anything, construct_index
-from src.components.resume import DataManipulation
-from src.components.resume import neural_network
+from src.components.resume import DataManipulation, Neural_Net
+# from src.components.resume import neural_network
 
+from src.exception import CustomException
+from src.logger import logging
 
 import os
-
 
 app = Flask(__name__)
 
@@ -34,28 +35,22 @@ def hello_world():
 
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
-    print("Get")
+
     if request.method == "GET":
         return render_template("upload.html")
     else:
-        print("Post")
         pdf_file = request.files["pdf_file"]
+        
         if pdf_file:
-            print('load pdf')
+
             pdf_file_path = os.path.join(app.config["UPLOAD_FOLDER"], pdf_file.filename)
-            print('save pdf')
+
             pdf_file.save(pdf_file_path)
-            print('extract')
+
             pdf_text = extract_pdf(pdf_file_path)
-            print(pdf_text)
-            # print(pdf_file_path)
-            # text = extract_text(str(pdf_file_path))
-            # text_replace = text.replace('\n',' ')
-            # return text_replace
+
             skills = []
             
-            print('build model')
-
             # Load pipeline
             model_name = "ml6team/keyphrase-extraction-distilbert-inspec"
             extractor = KeyphraseExtractionPipeline(model=model_name)
@@ -66,7 +61,6 @@ def upload():
             
             tokenizer = AutoTokenizer.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
             model = AutoModelForTokenClassification.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
-            print('keyprae')
             
             keyphrases_2 = ner(pdf_text, model, tokenizer)
             
@@ -74,33 +68,34 @@ def upload():
                 # print(keyphrases[i])
                 if keyphrases_2[i]['entity_group'] == 'MISC':
                     skills.append(keyphrases_2[i]['word'])
-                    print(keyphrases_2[i]['word'])
+                    # print(keyphrases_2[i]['word'])
                     
             skills = np.array(skills)
             keyphrases = np.concatenate((keyphrases_1,skills))
             print(keyphrases)
             keyphrases = list(keyphrases)
             
-            
+            logging.info('Keyphrases Token been generated')
             
             skills_df = pd.read_csv("jobs&skills.csv")
             roles = skills_df['job_role'].unique()
             
-            job = DataManipulation
-            all_data  = job.job_data(roles, skills_df)
+            job = DataManipulation(roles, skills_df)
+            all_data  = job.job_data()
             print("ALL DATA")
-            print(all_data)
+            # print(all_data)
+            job.jobs_skills_final_csv()
             
-            job.jobs_skills_final_csv(roles, skills_df)
+            logging.info('Job Statistics Related to Roles Generated')
             
             jobs_skills_final = pd.read_csv('jobs_skills_final.csv')
-            neural_network(jobs_skills_final)
+            Neural_Object = Neural_Net(jobs_skills_final, roles, skills_df)
+            Neural_Object.train()
             
             return render_template("upload.html", keyphrases=keyphrases, all_data=all_data)
 
             
-            
-            # return 'PDF file uploaded and saved successfull   y!'
+            # return 'PDF file uploaded and saved successfully!'
         else:
             return "No PDF file uploaded"
 
@@ -111,15 +106,20 @@ def answer():
         return render_template("chatbot.html")
 
     else:
-        # construct_index(r"textdata")
+        if 'index.json' in os.listdir():
+            logging.info('index.json file exists in the directory')
+        else:
+            logging.info('index.json file does not exist in the directory')
+            construct_index(r"textdata")
 
         question = request.form["question"]
 
         while question != "Exit":
             answer = ask_me_anything(question)
+            logging.info('Got answer')
             return render_template("chatbot.html", answer=answer)
             question = input("Enter a question (Type Exit to Exit from the chatbot): ")
-            
+        
         # Do something to get the answer based on the question
         # answer = "The answer to your question is..."
 
