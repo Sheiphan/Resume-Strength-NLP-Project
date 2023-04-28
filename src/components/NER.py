98d261005041379f8a7464151073b14c5ba17664
Sheiphan <sheiphanshaijan@gmail.com>
2023-04-29 05:21:30 +0530

    Chatbot, NER, resume updated logging & exception.

--- src/components/NER.py
+++ src/components/NER.py
@@ -1,13 +1,36 @@
 from pdfminer.high_level import extract_pages, extract_text
 import numpy as np
+import sys
+from typing import List, Dict, Union
 from transformers import (TokenClassificationPipeline, AutoModelForTokenClassification, AutoTokenizer)
 from transformers.pipelines import AggregationStrategy
+from transformers import pipeline
+
+from src.exception import CustomException
+from src.logger import logging
 
 
 def extract_pdf(file_path):
-    text = extract_text(file_path)
-    text_replace = text.replace('\n',' ')
+    """
+    Extracts text from a pdf file at the given file path and replaces new lines with spaces.
+
+    Args:
+    - file_path: A string representing the path of the pdf file to extract text from.
+
+    Returns:
+    - A string representing the extracted text from the pdf file with new lines replaced with spaces.
+    """
+    try : 
+        text = extract_text(file_path)
+    except Exception as e :
+        CustomException(e,sys)
+        
+    text_replace = text.replace('\n', ' ')
+    
+    logging.info('Extracts text from a pdf file at the given file path and replaces new lines with spaces.')
+
     return text_replace
+
 
 
 
@@ -19,21 +42,42 @@ def extract_pdf(file_path):
 
 ##### Process text sample (from wikipedia)
 
-from transformers import pipeline
 
-def ner(text_replace,model,tokenizer):
+def ner(text_replace, model, tokenizer):
+    """
+    Uses a pre-trained named entity recognition (NER) model to extract unique keyphrases from the text.
+    
+    Args:
+        text_replace (str): The text to extract keyphrases from.
+        model: The pre-trained NER model to use.
+        tokenizer: The tokenizer to use with the NER model.
+    
+    Returns:
+        list: A list of unique keyphrases extracted by the pipeline.
+    """
     nlp = pipeline('ner', model=model, tokenizer=tokenizer, aggregation_strategy="simple")
-
+    
+    logging.info('A list of unique keyphrases extracted by the pipeline from Jean-Baptiste/roberta-large-ner-english')
+    
     return nlp(text_replace)
 
 
 
 
-
-
 # Define keyphrase extraction pipeline
+
+
 class KeyphraseExtractionPipeline(TokenClassificationPipeline):
-    def __init__(self, model, *args, **kwargs):
+    """
+    A pipeline for keyphrase extraction using token classification.
+
+    Args:
+        model (str): The name of the pretrained model to use.
+        *args: Additional positional arguments to pass to the parent class.
+        **kwargs: Additional keyword arguments to pass to the parent class.
+    """
+
+    def __init__(self, model: str, *args, **kwargs):
         super().__init__(
             model=AutoModelForTokenClassification.from_pretrained(model),
             tokenizer=AutoTokenizer.from_pretrained(model),
@@ -41,12 +85,41 @@ class KeyphraseExtractionPipeline(TokenClassificationPipeline):
             **kwargs
         )
 
-    def postprocess(self, all_outputs):
+    def postprocess(self, all_outputs: List[Dict[str, Union[int, str]]]) -> List[str]:
+        """
+        Postprocess the outputs of the pipeline.
+
+        Args:
+            all_outputs (List[Dict[str, Union[int, str]]]): A list of dictionaries containing the outputs of the pipeline.
+
+        Returns:
+            List[str]: A list of unique keyphrases extracted by the pipeline.
+        """
         results = super().postprocess(
             all_outputs=all_outputs,
             aggregation_strategy=AggregationStrategy.FIRST,
         )
+        logging.info('A list of unique keyphrases extracted by the pipeline from ml6team/keyphrase-extraction-distilbert-inspec')
         return np.unique([result.get("word").strip() for result in results])
+
+
+
+# class KeyphraseExtractionPipeline(TokenClassificationPipeline):
+#     def __init__(self, model, *args, **kwargs):
+#         super().__init__(
+#             model=AutoModelForTokenClassification.from_pretrained(model),
+#             tokenizer=AutoTokenizer.from_pretrained(model),
+#             *args,
+#             **kwargs
+#         )
+
+#     def postprocess(self, all_outputs):
+#         results = super().postprocess(
+#             all_outputs=all_outputs,
+#             aggregation_strategy=AggregationStrategy.FIRST,
+#         )
+#         return np.unique([result.get("word").strip() for result in results])
+    
     
 # # Load pipeline
 # model_name = "ml6team/keyphrase-extraction-distilbert-inspec"
--- src/components/resume.py
+++ src/components/resume.py
@@ -1,22 +1,18 @@
 import pandas as pd
 import json
 import os
-
+from typing import List
 import numpy as np
-import pandas as pd
+
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
 from sklearn.preprocessing import LabelEncoder
-from keras.models import save_model
+from keras.models import Sequential, save_model
+from keras.layers import Dense, Dropout
 from keras.callbacks import EarlyStopping
 
+from src.exception import CustomException
+from src.logger import logging
 
-
-import matplotlib.pyplot as plt
-# !pip install mplcursors
-# import mplcursors
 
 
 
@@ -49,116 +45,153 @@ import matplotlib.pyplot as plt
 # skills_df = pd.read_csv("jobs&skills.csv")
 
 # roles = skills_df['job_role'].unique()
-class DataManipulation:
-    def job_data(roles, skills_df):
-        
-        all_job_data = dict()
 
-        for role in roles:
-            temp_df = skills_df[skills_df['job_role'] == role]
+class DataManipulation:
+    """
+    This class performs data manipulation on job roles and skills.
+    """
+
+    def __init__(self, roles: list, skills_df: pd.DataFrame):
+        """
+        Constructor for the DataManipulation class.
+
+        Parameters:
+        roles (list): List of job roles.
+        skills_df (pd.DataFrame): Dataframe of job roles and their respective skills.
+        """
+        self.roles = roles
+        self.skills_df = skills_df
+        self.job_data_dict = {}
+        self.jobs_skills_final_df = pd.DataFrame(columns=skills_df.columns)
+
+    def job_data(self) -> dict:
+        """
+        This method calculates the most important skill, percentage of most important skill, 
+        skills that are most important and least important for each job role.
+
+        Returns:
+        dict: Dictionary containing job roles, their most important skill and related information.
+        """
+        for role in self.roles:
+            temp_df = self.skills_df[self.skills_df['job_role'] == role]
             temp_df = temp_df.iloc[:,:-1]
             lst = temp_df.sum(axis=0)
             percentwise_counts = 100 * lst / len(temp_df)
             percentwise_counts = percentwise_counts.sort_values()
-            #  skills which are not present in any job posting for the role
-                    
-            print("\n\n",role)
             
             most_important_skill = percentwise_counts.idxmax()
             percent_of_MIS = percentwise_counts.max()
-            print("   most important skill: ",most_important_skill,"that is",percent_of_MIS,"%")
             
             skills_ab = [index for index, value in percentwise_counts.items() if value >= 0 and value <= 0.000000001]
-            print("   absent skills: ", len(skills_ab), skills_ab)
-
-            #Top skills required for each role
+            
             skills_most = [index for index, value in percentwise_counts.items() if value >= 40 and value <= 100]
-            print("   most common skills: ",len(skills_most), skills_most)
-
-            #Most rare skills for each role
+            
             skills_least = [index for index, value in percentwise_counts.items() if value >= 0.0000001 and value <= 3]
-            print("   most rare and useless skills: ",len(skills_least),skills_least)
             
-            one_data = [most_important_skill, percent_of_MIS,
-                    len(skills_most), skills_most,
-                    ]
+            one_data = {'most_important_skill': most_important_skill, 
+                        'percent_of_MIS': percent_of_MIS,
+                        'num_skills_most': len(skills_most), 
+                        'skills_most': skills_most}
             
-            all_job_data.update({f'{role}': 
-                                            {'most_important_skill' : most_important_skill,
-                                             'percent_of_MIS': percent_of_MIS,
-                                             'num_skills_most' : len(skills_most),
-                                             'skills_most' : skills_most,
-                                             } 
-                                 }
-                                )
+            self.job_data_dict[role] = one_data
             
-        return all_job_data
+            
+        logging.info('Dictionary containing job roles, their most important skill and related information.')
 
-
-    # creating df who has dropped skills which are for less than 3% of job postings of that role
-    def jobs_skills_final_csv(roles,skills_df):
-        jobs_skills_final = pd.DataFrame(columns=skills_df.columns)
-
-        for role in roles:
-            temp_df = skills_df[skills_df['job_role'] == role]
+        return self.job_data_dict
+    
+    def jobs_skills_final_csv(self) -> None:
+        """
+        This method creates a csv file of the final dataframe containing job roles and their respective skills.
+        """
+        for role in self.roles:
+            temp_df = self.skills_df[self.skills_df['job_role'] == role]
         
             lst = temp_df.iloc[:,:-1].sum(axis=0)
             percentwise_counts = 100 * lst / len(temp_df)
         
-            # Most rare skills for each role
             skills_least = [index for index, value in percentwise_counts.items() if value >= 0.0000001 and value <= 3]
         
-            # Set least common skills to 0
             temp_df[skills_least] = 0
             
-            # Append modified DataFrame to jobs_skills_final
-            jobs_skills_final = jobs_skills_final.append(temp_df)
+            self.jobs_skills_final_df = self.jobs_skills_final_df.append(temp_df)
 
-        # Reset index of final DataFrame
-        jobs_skills_final.reset_index(drop=True, inplace=True)
+        self.jobs_skills_final_df.reset_index(drop=True, inplace=True)
         
-        jobs_skills_final.to_csv('jobs_skills_final.csv', index=False)
+        self.jobs_skills_final_df.to_csv('jobs_skills_final.csv', index=False)
         
+        logging.info('Creates a csv file of the final dataframe containing job roles and their respective skills.')
+
+
 
 
 # job_data(roles, skills_df)
 
-def neural_network(jobs_skills_final):
-    le = LabelEncoder()
-    jobs_skills_final['job_role'] = le.fit_transform(jobs_skills_final['job_role'])
 
-    from keras.models import Sequential
-    from keras.layers import Dense, Dropout
-    from sklearn.model_selection import train_test_split
+class Neural_Net(DataManipulation):
+    """
+    Neural network class to prepare data and train a model for job classification.
+    """
+    def __init__(self, jobs_skills_final: pd.DataFrame, roles: List[str], skills_df: pd.DataFrame):
+        """
+        Initialize a Neural_Net object with jobs_skills_final, roles and skills_df dataframes.
 
-    # Prepare the data
-    X = jobs_skills_final.iloc[:, :-1].values
-    y = jobs_skills_final.iloc[:, -1].values
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # Define the model architecture
-    model = Sequential()
-    model.add(Dense(256, input_dim=X_train.shape[1], activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(len(np.unique(y)), activation='softmax'))
-
-    # Compile the model
-    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    # Define the early stopping callback
-    early_stop = EarlyStopping(monitor='val_loss', patience=3, verbose=1)
-
-    # Train the model with early stopping
-    history = model.fit(X_train, y_train, epochs=30, batch_size=32, validation_data=(X_test, y_test), callbacks=[early_stop])
-
-    # Save the model
-    save_model(model, 'model.h5')
-
-# jobs_skills_final = pd.read_csv('jobs_skills_final.csv')
-
-# neural_network(jobs_skills_final)
+        Parameters:
+        -----------
+        jobs_skills_final : pandas dataframe
+            A dataframe containing job roles and skills.
+        roles : list of str
+            A list of job roles.
+        skills_df : pandas dataframe
+            A dataframe containing skills and their corresponding level of expertise.
+        """
+        super().__init__(roles, skills_df)
+        self.jobs_skills_final = jobs_skills_final
+        self.le = LabelEncoder()
+        
+    def _prepare_data(self):
+        """
+        Prepare the data for training and testing the model.
+        """
+        self.jobs_skills_final['job_role'] = self.le.fit_transform(self.jobs_skills_final['job_role'])
+        X = self.jobs_skills_final.iloc[:, :-1].values
+        y = self.jobs_skills_final.iloc[:, -1].values
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        
+    def _define_model(self):
+        """
+        Define the neural network model architecture.
+        """
+        self.model = Sequential()
+        self.model.add(Dense(256, input_dim=self.X_train.shape[1], activation='relu'))
+        self.model.add(Dropout(0.2))
+        self.model.add(Dense(128, activation='relu'))
+        self.model.add(Dropout(0.2))
+        self.model.add(Dense(64, activation='relu'))
+        self.model.add(Dropout(0.2))
+        self.model.add(Dense(len(np.unique(self.y_train)), activation='softmax'))
+        
+    def _compile_model(self):
+        """
+        Compile the neural network model.
+        """
+        self.model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        
+    def _define_callbacks(self):
+        """
+        Define the callbacks for the neural network model.
+        """
+        self.callbacks = [EarlyStopping(monitor='val_loss', patience=3, verbose=1)]
+        
+    def train(self):
+        """
+        Train the neural network model and save it to disk.
+        """
+        self._prepare_data()
+        self._define_model()
+        self._compile_model()
+        self._define_callbacks()
+        history = self.model.fit(self.X_train, self.y_train, epochs=30, batch_size=32, validation_data=(self.X_test, self.y_test), callbacks=self.callbacks)
+        save_model(self.model, 'model.h5')
+        
+        logging.info('Train the neural network model and save it to disk.')
